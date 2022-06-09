8a159370ed1b8a5869002803de680757dcc75832
git-gabri <41841426+git-gabri@users.noreply.github.com>
2022-06-09 11:12:51 +0200

    Fixed typos, finished 3rd example

--- examples/sequence_detector.cpp
+++ examples/sequence_detector.cpp
@@ -1,5 +1,5 @@
 /*
-In this example, the fsm outputs a 1 whenever its input changes from 0 to 1 or viceversa.
+In this example, the fsm outputs a 1 whenever the input follows the sequence 10010.
 
 The input is a random boolean.
 */
@@ -13,48 +13,63 @@ using namespace std;
 
 int main(){
     srand(time(NULL));
-    moore_fsm fsm{1, 1, 4};
+    moore_fsm fsm{1, 1};
 
     //Signals
     bool boolean_in;
-    bool edge_detected;
+    bool sequence_detected;
 
     //Assign name aliases to inputs and outputs
     fsm.set_input_name(0, "input");
-    fsm.set_output_name(0, "edge");
+    fsm.set_output_name(0, "seq");
 
-    fsm.add_state(  "low_in_low_out",                   //name of the state
+    fsm.add_state(  "bit0",                             //name of the state
                     {0},                                //state outputs (in this case there's only one)
                     []tr_lamba -> size_t{  //lambda to calculate the next state
                         if(inputs[name_to_input_id.at("input")] != 0)
-                            return name_to_state_id.at("high_in_high_out");
+                            return name_to_state_id.at("bit1");
                         else
-                            return name_to_state_id.at("low_in_low_out");
+                            return name_to_state_id.at("bit0");
                     });
-
-    fsm.add_state(  "high_in_high_out",
-                    {1},
+    fsm.add_state(  "bit1",
+                    {0},
                     []tr_lamba -> size_t{
-                        if(inputs[name_to_input_id.at("input")] != 0)
-                            return name_to_state_id.at("high_in_low_out");
+                        if(inputs[name_to_input_id.at("input")] == 0)
+                            return name_to_state_id.at("bit2");
                         else
-                            return name_to_state_id.at("low_in_high_out");
+                            return name_to_state_id.at("bit1");
                     });
-    fsm.add_state(  "high_in_low_out",
+    fsm.add_state(  "bit2",
+                    {0},
+                    []tr_lamba -> size_t{
+                        if(inputs[name_to_input_id.at("input")] == 0)
+                            return name_to_state_id.at("bit3");
+                        else
+                            return name_to_state_id.at("bit1");
+                    });
+    fsm.add_state(  "bit3",
                     {0},
                     []tr_lamba -> size_t{
                         if(inputs[name_to_input_id.at("input")] != 0)
-                            return name_to_state_id.at("high_in_low_out");
+                            return name_to_state_id.at("bit4");
                         else
-                            return name_to_state_id.at("low_in_high_out");
+                            return name_to_state_id.at("bit0");
                     });
-    fsm.add_state(  "low_in_high_out",
+    fsm.add_state(  "bit4",
+                    {0},
+                    []tr_lamba -> size_t{
+                        if(inputs[name_to_input_id.at("input")] == 0)
+                            return name_to_state_id.at("seq");
+                        else
+                            return name_to_state_id.at("bit1");
+                    });
+    fsm.add_state(  "seq",
                     {1},
                     []tr_lamba -> size_t{
                         if(inputs[name_to_input_id.at("input")] != 0)
-                            return name_to_state_id.at("high_in_high_out");
+                            return name_to_state_id.at("bit1");
                         else
-                            return name_to_state_id.at("low_in_low_out");
+                            return name_to_state_id.at("bit0");
                     });
 
     //Initial conditions
@@ -64,18 +79,19 @@ int main(){
 	cout << "- FSM state  : " << fsm.get_current_state_name() << " (" << fsm.get_current_state_id() << ")" << endl;
 	cout << "- FSM output : " << fsm.get_output(0) << endl;
     boolean_in = false;
-    edge_detected = fsm.get_output(0);
+    sequence_detected = fsm.get_output(0);
     cout << "- signal \"boolean_in\"    : " << boolean_in << endl;
-    cout << "- signal \"edge_detected\" : " << edge_detected << endl;
+    cout << "- signal \"sequence_detected\" : " << sequence_detected << endl;
 
-    //Simulation of the circuit for 30 clock cycles
+    //Simulation of the circuit until the sequence is detected 2 times
     vector<bool> boolean_inputs;
     boolean_inputs.reserve(100);
     vector<bool> edges;
     edges.reserve(100);
     boolean_inputs.push_back(boolean_in);
-    edges.push_back(edge_detected);
-    for(int i = 0; i < 30; ++i){
+    edges.push_back(sequence_detected);
+    int num_detects = 0;
+    while(num_detects < 2){
         //The simulation is divided into 3 phases:
         //- simulation of the fsm and of the datapath
         //- update of the signals based on the output of the devices
@@ -86,7 +102,9 @@ int main(){
         boolean_in = (rand() % 2) && (rand() % 2);
 
         //Step 2: update the signals
-        edge_detected = fsm.get_output("edge");
+        sequence_detected = fsm.get_output("seq");
+        if(sequence_detected)
+            ++num_detects;
 
         //Step 3: update the inputs of the devices, in this case only of the fsm
         fsm.set_input(0, boolean_in);
@@ -97,23 +115,16 @@ int main(){
 	    //cout << "- FSM state  : " << fsm.get_current_state_name() << " (" << fsm.get_current_state_id() << ")" << endl;
 	    //cout << "- FSM output : " << fsm.get_output(0) << endl;
         //cout << "- signal \"boolean_in\"    : " << boolean_in << endl;
-        //cout << "- signal \"edge_detected\" : " << edge_detected << endl;
+        //cout << "- signal \"sequence_detected\" : " << sequence_detected << endl;
 
         //Collect info in vector
         boolean_inputs.push_back(boolean_in);
-        edges.push_back(edge_detected);
+        edges.push_back(sequence_detected);
     }
 
     //Display info collected in vectors
-    cout << "                 if these two bits are different..." << endl;
-    cout << "                 ||" << endl;
-    cout << "                 VV" << endl;
-    cout << "Boolean inputs : "; for(auto it = boolean_inputs.begin(); it != boolean_inputs.end(); ++it) cout << (*it ? "1" : "0"); cout << endl;
-    cout << "Edges detected : "; for(auto it = edges.begin(); it != edges.end(); ++it) cout << (*it ? "1" : "0"); cout << endl;
-    cout << "Predicted      : 00"; for(auto it = boolean_inputs.begin() + 2; it != boolean_inputs.end(); ++it) cout << (*(it-1) != *(it-2) ? "1" : "0"); cout << endl;
-    cout << "                   ^" << endl;
-    cout << "                   |" << endl;
-    cout << "              ...then this bit is 1, otherwise it's 0" << endl;
+    cout << "Boolean inputs     : "; for(auto it = boolean_inputs.begin(); it != boolean_inputs.end(); ++it) cout << (*it ? "1" : "0"); cout << endl;
+    cout << "Sequences detected : "; for(auto it = edges.begin(); it != edges.end(); ++it) cout << (*it ? "1" : "0"); cout << endl;
 
     return 0;
 }
